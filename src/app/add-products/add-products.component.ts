22f004de11dc97c23cd9a136ca3cc85caa983ebf
Varun-Babu <itsme.varun.26@gmail.com>
2022-11-28 16:03:27 +0530

    binded

--- src/app/add-products/add-products.component.ts
+++ src/app/add-products/add-products.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-products.component.css']
 })
 export class AddProductsComponent {
+  title =""
+  description=""
+  price=""
+  category=""
+  image=""
+  rating=""
+  rate=""
+  count=""
+  seller=""
+  quantity=""
+
+
+
+  readValue = () =>
+  {
+    let data:any = {"title":this.title,"price":this.price,"description":this.description,"category":this.category,"image":this.image,"rating":{"rate":this.rate,"count":this.count},"seller":this.seller,"quantity":this.quantity}
+    console.log(data)
+  }
+
 
 }
